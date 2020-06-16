c2a24e7b452c2d4faed270c0997884647cdb66d2
Szymon Lichon <szymonln@gmail.com>
2020-06-16 21:07:13 +0200

    Remove static files handling - problems with heroku:

--- sharedLaundry/settings.py
+++ sharedLaundry/settings.py
@@ -117,8 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_URL = '/static/'
